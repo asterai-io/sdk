c36ac83a5702627aa46f9827ea661e88c97c5688
Lorenzo Fontoura <lorenzo@nioel.com>
2024-11-03 00:56:52 +1000

    fix: write/read buffer

--- go/buffer.go
+++ go/buffer.go
@@ -2,35 +2,26 @@ package sdk
 
 import (
 	"encoding/binary"
-	"reflect"
 	"unsafe"
 )
 
 func WriteBuffer(buffer []byte) uint32 {
-	ptr := heapAlloc(uint32(len(buffer)) + 4)
-	// Write the length as a uint32 at the start of the allocated memory.
-	binary.LittleEndian.PutUint32(
-		(*[1 << 30]byte)(unsafe.Pointer(uintptr(ptr)))[:], uint32(len(buffer)),
-	)
+	bufferLen := uint32(len(buffer))
+	ptr := heapAlloc(bufferLen + 4)
+	binary.LittleEndian.PutUint32((*[4]byte)(unsafe.Pointer(uintptr(ptr)))[:], bufferLen)
 	copyToPtr(ptr+4, buffer)
 	return ptr
 }
 
 func copyToPtr(ptr uint32, buffer []byte) {
-	// Convert uint32 pointer to uintptr for use with unsafe operations.
 	destination := unsafe.Pointer(uintptr(ptr))
-	// Convert slice to a pointer to its underlying array.
-	source := unsafe.Pointer((*reflect.SliceHeader)(unsafe.Pointer(&buffer)).Data)
-	// Perform the copy.
+	source := unsafe.Pointer(&buffer[0])
 	copy((*[1 << 30]byte)(destination)[:len(buffer)], (*[1 << 30]byte)(source)[:len(buffer)])
 }
 
 func ReadBuffer(ptr uint32) []byte {
-	// Convert the uint32 pointer to a uintptr for use with unsafe operations.
 	data := unsafe.Pointer(uintptr(ptr))
-	// Read the length (uint32).
 	length := binary.LittleEndian.Uint32((*[4]byte)(data)[:])
-	// Now read the actual data, skipping the first 4 bytes used for length.
 	return (*[1 << 30]byte)(unsafe.Add(data, 4))[:length:length]
 }
 
