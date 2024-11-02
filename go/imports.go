08d79b75fbb33899ff35649c6ad6982723d193bb
Lorenzo Fontoura <lorenzo@nioel.com>
2024-11-02 16:02:42 +1000

    chore: name wasm module

--- go/imports.go
+++ go/imports.go
@@ -1,7 +1,9 @@
 package sdk
 
+//go:wasm-module index
 //export index.host.log
 func hostLog(request uint32)
 
+//go:wasm-module index
 //export index.host.http_request
 func hostHttpRequest(request uint32) uint32
