5fe0b176b2d5c1ebfd1ee56b469f280ee39bf1f9
Lorenzo Fontoura <lorenzo@nioel.com>
2024-11-03 03:58:18 +1000

    chore: remove export directive

--- go/buffer.go
+++ go/buffer.go
@@ -25,14 +25,12 @@ func ReadBuffer(ptr uint32) []byte {
 	return (*[1 << 30]byte)(unsafe.Add(data, 4))[:length:length]
 }
 
-//export allocate
 //go:wasmexport allocate
 func heapAlloc(len uint32) uint32 {
 	slice := make([]byte, len)
 	return *(*uint32)(unsafe.Pointer(&slice))
 }
 
-//export deallocate
 //go:wasmexport deallocate
 func heapDealloc(_len uint32) {
 	// This is just a placeholder.
