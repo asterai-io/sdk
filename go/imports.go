f6474427ec1dc438a4e901e606b150bb96177e56
Lorenzo Fontoura <lorenzo@nioel.com>
2024-11-03 03:48:49 +1000

    chore: add regular go compiler wasm directives

--- go/imports.go
+++ go/imports.go
@@ -2,8 +2,10 @@ package sdk
 
 //go:wasm-module index
 //export host.log
+//go:wasmimport index host.http_request
 func hostLog(request uint32)
 
 //go:wasm-module index
 //export host.http_request
+//go:wasmimport index host.http_request
 func hostHttpRequest(request uint32) uint32
