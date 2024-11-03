d52e1c7282d8d547d403e3ce2e3261a2617195ef
Lorenzo Fontoura <lorenzo@nioel.com>
2024-11-03 18:25:36 +1000

    chore: use regular export

--- go/buffer.go
+++ go/buffer.go
@@ -25,13 +25,13 @@ func ReadBuffer(ptr uint32) []byte {
 	return (*[1 << 30]byte)(unsafe.Add(data, 4))[:length:length]
 }
 
-//go:wasmexport allocate
+//export allocate
 func heapAlloc(len uint32) uint32 {
 	slice := make([]byte, len)
 	return *(*uint32)(unsafe.Pointer(&slice))
 }
 
-//go:wasmexport deallocate
+//export deallocate
 func heapDealloc(_len uint32) {
 	// This is just a placeholder.
 	// The GC deallocs.
