21d6368ccaf388fdb72f4297f1498fd66a717b3e
Lorenzo Fontoura <lorenzo@nioel.com>
2024-11-07 10:38:51 +1000

    fix: export correctly

--- go/imports.go
+++ go/imports.go
@@ -10,6 +10,7 @@ func hostLog(request uint32)
 //go:wasmimport index host.http_request
 func hostHttpRequest(request uint32) uint32
 
-//go:wasmimport plugin_env_get_string
 //go:wasm-module index
+//export host.plugin_env_get_string
+//go:wasmimport index host.plugin_env_get_string
 func hostPluginEnvGetString(request uint32) uint32
