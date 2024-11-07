3ce2e394a4f6e4c454dfe4ce03c6a47d3369a7ca
Lorenzo Fontoura <lorenzo@nioel.com>
2024-11-07 10:32:07 +1000

    feat: get env var

--- go/imports.go
+++ go/imports.go
@@ -9,3 +9,7 @@ func hostLog(request uint32)
 //export host.http_request
 //go:wasmimport index host.http_request
 func hostHttpRequest(request uint32) uint32
+
+//go:wasmimport plugin_env_get_string
+//go:wasm-module index
+func hostPluginEnvGetString(request uint32) uint32
