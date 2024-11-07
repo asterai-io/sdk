e19aa7aaa221fbfcb75c37bc392cb5878510b301
Lorenzo <lorenzo@nioel.com>
2024-11-07 13:48:23 +1000

    feat(go): user kv imports

--- go/imports.go
+++ go/imports.go
@@ -14,3 +14,13 @@ func hostHttpRequest(request uint32) uint32
 //export host.plugin_env_get_string
 //go:wasmimport index host.plugin_env_get_string
 func hostPluginEnvGetString(request uint32) uint32
+
+//go:wasm-module index
+//export host.kv_get_user_string
+//go:wasmimport index host.kv_get_user_string
+func hostKvGetUserString(request uint32) uint32
+
+//go:wasm-module index
+//export host.kv_set_user_string
+//go:wasmimport index host.kv_set_user_string
+func hostKvSetUserString(request uint32)
