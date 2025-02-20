dc2a18cd6cbb430667a058e02f97c36e19f93223
Lorenzo Fontoura <lorenzo@nioel.com>
2025-02-21 05:31:38 +1000

    chore: update init example

--- cli/asterai-init-plugin/plugin.ts
+++ cli/asterai-init-plugin/plugin.ts
@@ -1,9 +1,11 @@
-import { BinaryOperationInput, Output } from "./typegen/plugin";
+import { BinaryOperationInput } from "./generated/plugin";
+import * as asterai from "asterai:host/api@0.1.0";
 
-export const add = (input: BinaryOperationInput): Output => {
+export const add = (input: BinaryOperationInput): number => {
   const result = input.a + input.b;
-  // The `systemMessage` field is sent to the LLM.
-  return {
-    systemMessage: `the result is ${result}`,
-  };
+  // Send the calculation result to the agent.
+  asterai.sendResponseToAgent(`the result is ${result}`);
+  // This result is not seen by the agent, but it can be consumed by
+  // other plugins calling this function.
+  return result;
 };
