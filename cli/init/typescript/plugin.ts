a8aeaa311031def941a97a4cc2d0b38f971b39d6
Lorenzo Fontoura <lorenzo@nioel.com>
2025-03-17 02:17:50 +1000

    fix: update jco to fix typegen in typescript init

--- cli/init/typescript/plugin.ts
+++ cli/init/typescript/plugin.ts
@@ -1,11 +1,13 @@
-import { BinaryOperationInput } from "./generated/plugin";
 import * as asterai from "asterai:host/api@0.1.0";
+import { BinaryOperationResult } from "example:math/plugin@0.1.0";
 
-export const add = (input: BinaryOperationInput): number => {
-  const result = input.a + input.b;
+export const mul = (a: number, b: number): BinaryOperationResult => {
+  const result = a + b;
   // Send the calculation result to the agent.
   asterai.sendResponseToAgent(`the result is ${result}`);
   // This result is not seen by the agent, but it can be consumed by
   // other plugins calling this function.
-  return result;
+  return {
+    value: result,
+  };
 };
